c4f1e86a09665855085573a1594a0f394b010d47
Nap199I <nathan_parry15@hotmail.com>
2023-02-24 11:59:12 +0000

    Initial commit

--- map.js
+++ map.js
@@ -6,15 +6,32 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
-// Using map()
+// Using map() (same as above using the map method)
 
+const multiplyByTwo = (num) => num * 2;
+
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const Simplified = nums.map( function (num) {return num * 2});
 
-// Simplfied w/ map() + arrow function
+console.log(Simplified);
 
+
+// Simplfied w/ map() + arrow function (the function num doesnt need the paranthases because it only takes one value)
+
+const mapResults2 = nums.map(num => num * 2);
+
+console.log(mapResults2);
 
 // With objects:
 const students = [
@@ -37,3 +54,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map( student => [student.name, student.id] );
+console.log(studentsWithIds);
